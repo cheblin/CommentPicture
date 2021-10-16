2178b06c383fa6daa0fcb830cf0c5ab3898e3d51
GitHub Action <action@github.com>
2021-10-16 03:39:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/cheblin/commentpicture/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/cheblin/commentpicture/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.cheblin.commentpicture.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.cheblin.commentpicture.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
